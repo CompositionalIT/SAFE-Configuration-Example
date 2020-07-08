cdd8fbfe5e8b8127b8c0ff4c8ead7b989c7d2ea2
Ryan Palmer <ryan@compositional-it.com>
2020-07-08 19:34:22 +0100

    Manually bolt Secrets on to the end of the default config. Shouldn't be necessary and doesn't seem to make a difference

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -47,9 +47,16 @@ let webApp next ctx =
         return! handler next ctx
     }
 
+let addSecretsConfig cfg =
+    ConfigurationBuilder()
+        .AddConfiguration(cfg)
+        .AddUserSecrets<AnyType>()
+        .Build()
+
 let app =
     application {
         url "http://0.0.0.0:8085"
+        use_config (fun cfg -> addSecretsConfig cfg)
         use_router webApp
         memory_cache
         use_static "public"
